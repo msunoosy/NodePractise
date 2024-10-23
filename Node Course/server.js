c9df4a268a1238410eb1a299d975d6cc3c6c66e1
zamzamy Mohamed sunoosy <m.sunoos@gmail.com>
2024-10-23 21:39:13 +0400

    Bug fixed

--- Node Course/server.js
+++ Node Course/server.js
@@ -1,10 +1,18 @@
-const { ifError } = require('assert');
-const { error } = require('console');
 const http=require('http');
-const PORT=3000;
+const PORT=3001;
+const fs=require('fs')
 
 const server=http.createServer((req,res)=>{
-
+    res.writeHead(200,{'content-Type':'text/html'})
+    fs.readFile('index.html',(error,data)=>{
+        if(error){
+            res.writeHead(400)
+            res.write('PAGE NOT FOUND.....')
+        }else{
+            res.write(data)
+        }
+    })
+      res.end()
 })
 
 server.listen(PORT,(error)=>{
